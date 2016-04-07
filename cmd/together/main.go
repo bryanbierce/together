f57a3c4dc26ef553bb18d8dba95a19e69fdb709b
Bryan Bierce <bryanbierce@gmail.com>
2016-04-07 13:21:04 -0700

    Altered public route, added favicon

--- cmd/together/main.go
+++ cmd/together/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	http.HandleFunc("/api/", handleAPI(session))
 
-	http.Handle("/", http.FileServer(http.Dir("../../public")))
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 
 	fmt.Println("Server running on port 4028")
 	http.ListenAndServe(":"+port, nil)
