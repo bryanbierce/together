29c3bd539a3b005bfcb9a9a1590a417e7ad46821
Bryan Bierce <bryanbierce@gmail.com>
2016-04-27 18:31:28 -0700

    Upgraded react-dom to v15

--- webpack.config.js
+++ webpack.config.js
@@ -36,16 +36,16 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       compress: {
         warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
       }
     })
   ]
